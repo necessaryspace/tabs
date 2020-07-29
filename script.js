b6726173ad14aa644242b71d693c886c5d8c5085
necessaryspace <64280737+necessaryspace@users.noreply.github.com>
2020-07-29 22:44:25 +0500

    TABS JS

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', function () {
+   'use strict';
+   let tab = document.querySelectorAll('.info-header-tab'),
+      info = document.querySelector('.info-header'),
+      tabContent = document.querySelectorAll('.info-tabcontent');
+
+      function hideTabeContent(a) {
+         for(let i = a; i < tabContent.length; i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+         }
+      }
+      hideTabeContent(1);
+
+      function showTabContent(b) {
+         if(tabContent[b].classList.contains('hide')){
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+         }
+      }
+      info.addEventListener('click', function(event) {
+         let target = event.target;
+         if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++){
+               if(target == tab[i]){
+                  hideTabeContent(0);
+                  showTabContent(i);
+                  break;
+               }
+            }
+         }
+      });
+});
